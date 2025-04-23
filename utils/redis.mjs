766b527ea3e9adbe38a9d0dbe618f404156157b5
Louis Martinez Echevarria <louismartinezechevarria@gmail.com>
2025-04-23 11:09:51 -0400

    Better late...
    Than never
    
    Noticed that everything I did had the wrong directory name, had to upload all at once

---
+++ utils/redis.mjs
@@ -0,0 +1,41 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (err) => console.error('Redis Client Error:', err));
+        this.client.connect();
+    }
+
+    isAlive() {
+        return this.client.isReady;
+    }
+
+    async get(key) {
+        try {
+            return await this.client.get(key);
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+    }
+
+    async set(key, value, duration) {
+        try {
+            await this.client.set(key, value, { EX: duration });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async del(key) {
+        try {
+            await this.client.del(key);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
